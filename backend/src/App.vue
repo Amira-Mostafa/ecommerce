2c3fe49c2dbd05b9a4431812d9ddf0dcc58485de
Amira-Mostafa <amirazalazel@gmail.com>
2024-08-13 23:51:04 +0300

    Update:installing vuejs dependencies

---
+++ backend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template class="bg-purple-500">
+<router-view></router-view>
+</template>
+
+<style >
+</style>
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,54 @@
+
+<template>
+<div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+  
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" value="true">
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <div class="mt-2">
+              <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+  
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <div class="text-sm">
+                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+              </div>
+            </div>
+            <div class="mt-2">
+              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+  <div class="flex items-center justify-between">
+<div class="flex items-center">
+    <input id="remember-me" name="remember-me" type="checkbox" v-model="user.remember" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-grey-300 rounded" >
+  <label for="remember-me" class="ml-2 block text-sm text-grey-900"></label>
+</div>
+  </div>
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          </div>
+        </form>
+  
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Not a member?
+          {{ ' ' }}
+          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        </p>
+      </div>
+    </div>
+</div>
+  </template>
+  
+
+<style scoped>
+
+</style>
